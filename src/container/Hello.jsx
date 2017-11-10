5a05ab13c463aac1ab919680a513ff1e2c0aab19
ive <980425571@qq.com>
2017-11-10 15:06:59 +0800

    生命周期

--- src/container/Hello.jsx
+++ src/container/Hello.jsx
@@ -1,13 +1,27 @@
 import React from '@react';
 export default class Hello extends React.Component {
+	getDefaultProps() {
+		return {};
+	}
+	getInitialState() {
+		return {}
+	}
 	componentWillMount() {
-		console.log('Hello.componentWillMount...')
+		console.log('Hello componentWillMount...')
 	}
 	componentDidMount() {
-		console.log('Hello.componentDidMount...')
+		console.log('Hello componentWillMount...')	
+	}
+	componentWillReceiveProps(nextProps) {
+		console.log('Hello componentWillReceiveProps...')
+	}
+	componentWillUpdate(nextProps, nextState) {
+		console.log('Hello componentWillUpdate...')	
 	}
 	render(){
-		console.log('Hello.render...')
-		return <div hello="hello" style={{border: '1px red solid'}}>hello</div>;
+		console.log('Hello render...')
+		return (
+			<div>Hello...</div>
+		);
 	}
 }
